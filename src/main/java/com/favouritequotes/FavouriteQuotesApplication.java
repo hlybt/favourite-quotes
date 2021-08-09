77683236d06af9fda7056ced1b8cb9ae1bd91d3a
hlybt <hulyabt@gmail.com>
2021-08-09 12:14:24 +0200

    Add all files

---
+++ src/main/java/com/favouritequotes/FavouriteQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.favouritequotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouriteQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favouritequotes/FavouriteQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favouritequotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
